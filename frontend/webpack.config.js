9b7668d0f49023655c9670b6bd75bb473a3480cc
Alexey Navarkin <navarka.all@gmail.com>
2019-09-09 00:09:37 +0300

    Updated webpack configuration.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,21 +1,39 @@
+const webpack = require('webpack');
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: './js/index.js',
   output: {
-    filename: 'bumdle.js',
-    path: path.resolve(__dirname, 'dist/js')
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s[ac]ss$/i,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          'sass-loader',
         ]
-      }
-    ]
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CopyPlugin([
+      { from: 'index.html', to: '' },
+    ]),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: 'source-map',  // Source map generations
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    liveReload: true,
+    port: 8000,
   }
+  // watch: true`
 };
